30af54708e1ff7ee4837b78efdfac119d15920bc
eugeni.soto <eugeni.soto@srs.io>
2023-11-23 19:09:32 +0100

    prova

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,65 +3,81 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>¡La página más divertida del mundo!</title>
+    <title>Bienvenido a {{ config('app.name') }}</title>
     <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=Comic+Sans+MS&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
 
     <style>
         body {
-            background-color: #fffc00;
-            font-family: 'Comic Sans MS', cursive, sans-serif;
-            color: #003366;
+            margin: 0;
+            padding: 0;
+            font-family: 'Roboto', sans-serif;
+            background-color: #f4f4f4;
+            color: #333;
             text-align: center;
+            line-height: 1.6;
         }
-        .title {
-            color: #ff33cc;
-            text-shadow: 2px 2px 4px #000000;
+        .header {
+            background: #fff;
+            padding: 20px 0;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
         }
-        .funny-image {
-            display: block;
-            margin-left: auto;
-            margin-right: auto;
-            width: 50%;
+        .header h1 {
+            margin: 0;
+            font-weight: 700;
         }
-        .dancing-text {
-            animation: dance 2s infinite;
+        .navigation {
+            margin-top: 15px;
         }
-        @keyframes dance {
-            0%   { transform: translateX(0px); }
-            25%  { transform: translateX(10px); }
-            75%  { transform: translateX(-10px); }
-            100% { transform: translateX(0px); }
+        .navigation a {
+            margin: 0 10px;
+            color: #333;
+            text-decoration: none;
+            font-weight: 500;
+        }
+        .navigation a:hover {
+            text-decoration: underline;
+        }
+        .content {
+            padding: 40px 0;
         }
         .footer {
-            margin-top: 50px;
-            font-size: 14px;
-            color: #666666;
+            background: #fff;
+            padding: 20px 0;
+            position: fixed;
+            width: 100%;
+            bottom: 0;
+            box-shadow: 0 -2px 4px rgba(0, 0, 0, 0.1);
+        }
+        .footer p {
+            margin: 0;
+            color: #666;
         }
     </style>
 </head>
 <body>
-    <h1 class="title">¡Bienvenido a la página más divertida de {{ config('app.name') }}!</h1>
-    @if (Route::has('login'))
-        <div class="top-right links">
-            @auth
-                <a href="{{ url('/home') }}">Home</a>
-            @else
-                <a href="{{ route('login') }}">Login</a>
-                @if (Route::has('register'))
-                    <a href="{{ route('register') }}">Register</a>
-                @endif
-            @endauth
-        </div>
-    @endif
+    <div class="header">
+        <h1>Bienvenido a {{ config('app.name') }}</h1>
+        @if (Route::has('login'))
+            <div class="navigation">
+                @auth
+                    <a href="{{ url('/home') }}">Home</a>
+                @else
+                    <a href="{{ route('login') }}">Login</a>
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}">Register</a>
+                    @endif
+                @endauth
+            </div>
+        @endif
+    </div>
 
-    <p class="dancing-text">¿Estás listo para ver algo increíble?</p>
-    <img src="https://www.iebschool.com/blog/wp-content/uploads/2015/03/hacer-venta.gif" alt="Imagen divertida" class="funny-image">
-    
+    <div class="content">
+        <p>Explora la belleza y la simplicidad de {{ config('app.name') }}.</p>
+        <p>Construye algo increíble.</p>
+    </div>
+
     <div class="footer">
-        <p>Esta página fue creada para sacarte una sonrisa 😄</p>
-        <p>¡No olvides compartir la alegría!</p>
         <p>Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})</p>
     </div>
 </body>
